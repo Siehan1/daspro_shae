1f227c91e29194f7342072ab8d33a642fd6a2855
Siehan <113917900+Siehan1@users.noreply.github.com>
2023-09-18 00:07:01 +0700

    Add files via upload

---
+++ pertemuan 2/lingkaran.py
@@ -0,0 +1,10 @@
+phi = 3.14
+
+print("===== Menghitung Luas dan Keliling Lingkaran =====")
+print("==================================================")
+
+jari2 = float(input("Masukkan Jari Jari Lingkaran: "))
+print("==================================================")
+
+luas = phi * (jari2 ** 2)
+print("Luas Lingkaran adalah :",luas)
